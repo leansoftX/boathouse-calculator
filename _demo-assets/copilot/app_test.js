2b88bd21106875b898f8202cf4baaa5585ae4638
Lei Xu <leixu@leansoftx.com>
2023-04-09 11:29:36 +0800

    update

---
+++ _demo-assets/copilot/app_test.js
@@ -0,0 +1,28 @@
+function calculator(str) {
+  var reg = /^(\d+)(\+|-|\*|\/)(\d+)$/;
+  var result = reg.exec(str);
+  if (result) {
+    var num1 = parseInt(result[1]);
+    var num2 = parseInt(result[3]);
+    var operator = result[2];
+    var res = 0;
+    switch (operator) {
+      case "+":
+        res = num1 + num2;
+        break;
+      case "-":
+        res = num1 - num2;
+        break;
+      case "*":
+        res = num1 * num2;
+        break;
+      case "/":
+        res = num1 / num2;
+        break;
+    }
+    return res;
+  } else {
+    return "输入的字符串不符合要求";
+  }
+}
+
---
+++ _demo-assets/copilot/query.sql
@@ -0,0 +1,2 @@
+-- 对2个表进行关联查询，第一个表叫做user(userId, userName, password)，第二个表叫做product(productId, productName, price, userId)
+-- 查询出所有用户的用户名和密码，以及用户购买的商品名称和价格
